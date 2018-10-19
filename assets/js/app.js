c931b8830372f2e669fb0217f23b92f4937c7edc
Jakob Unnebäck <jakob@folkdatorn.se>
2018-10-19 14:57:48 +0200

    Improved slideshow

--- assets/js/app.js
+++ assets/js/app.js
@@ -33,6 +33,7 @@ function initiatePage () {
   highlightDayOfTheWeek()
   populateHairdresserContainer()
   animateContainer(true, '#who')
+  window.setTimeout(function () { autoScrollSlideshow() }, 4000)
 }
 
 function getToday () {
@@ -455,7 +456,38 @@ function newBooking () {
 }
 
 function slideshow (goTo) {
-  document.querySelector('#imageContainer').style.transform = 'translateX(' + -25 * (goTo - 1) + '%)'
+  const container = document.querySelector('#imageContainer')
+  const oldDot = document.querySelector('#page3 #dots .active')
+  const childs = document.querySelectorAll('#dots a')
+  if (goTo === undefined && childs[0]) {
+    const current = document.querySelector('#dots .active')
+    for (let i = 0; i < childs.length; i++) {
+      if (childs[i] === current) {
+        goTo = i + 2
+        if (goTo > childs.length) {
+          goTo = 1
+        }
+      }
+    }
+  }
+  const dot = document.querySelector('#slideshow' + goTo)
+  if (oldDot) {
+    oldDot.classList.remove('active')
+  }
+  if (dot) {
+    dot.classList.add('active')
+  }
+  if (container) {
+    container.style.transform = 'translateX(' + -25 * (goTo - 1) + '%)'
+  }
+}
+
+function autoScrollSlideshow () {
+  const loop = function () {
+    slideshow()
+    window.setTimeout(function () { loop() }, 4000)
+  }
+  loop()
 }
 
 /* Export public functions */
